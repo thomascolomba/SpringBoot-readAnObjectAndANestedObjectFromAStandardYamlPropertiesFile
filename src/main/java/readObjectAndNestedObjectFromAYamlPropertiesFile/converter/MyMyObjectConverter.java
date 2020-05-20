422ff7aa08bead8d885fa22ddbae1ab11b180bb7
thomas <thomas.colomba30@gmail.com>
2020-05-20 19:00:44 +0200

    factorize doce in Converters

--- src/main/java/readObjectAndNestedObjectFromAYamlPropertiesFile/converter/MyMyObjectConverter.java
+++ src/main/java/readObjectAndNestedObjectFromAYamlPropertiesFile/converter/MyMyObjectConverter.java
@@ -6,23 +6,13 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesBindin
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import readObjectAndNestedObjectFromAYamlPropertiesFile.configuration.MyConfigurationBean.MyObject;
 
 @Component
 @ConfigurationPropertiesBinding
 public class MyMyObjectConverter implements Converter<LinkedHashMap<String, String> , MyObject> {
-    @Override
+	@Override
     public MyObject convert(LinkedHashMap<String, String> sourceFromJson) {
-        if (sourceFromJson == null) {
-            return null;
-        }
-        return copyHashMapContentIntoEquivalentMyObject(sourceFromJson);
-    }
-    
-    private MyObject copyHashMapContentIntoEquivalentMyObject(LinkedHashMap<String, String> myObjectFromJson) {
-    	final ObjectMapper mapper = new ObjectMapper();
-        return mapper.convertValue(myObjectFromJson, MyObject.class);
+        return (MyObject) MyObjectConverterUtil.copyHashMapContentIntoEquivalentMyObject(sourceFromJson, MyObject.class);
     }
 }
--- src/main/java/readObjectAndNestedObjectFromAYamlPropertiesFile/converter/MyMySecondObjectConverter.java
+++ src/main/java/readObjectAndNestedObjectFromAYamlPropertiesFile/converter/MyMySecondObjectConverter.java
@@ -6,8 +6,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesBindin
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import readObjectAndNestedObjectFromAYamlPropertiesFile.configuration.MyConfigurationBean.MySecondObject;
 
 @Component
@@ -15,15 +13,6 @@ import readObjectAndNestedObjectFromAYamlPropertiesFile.configuration.MyConfigur
 public class MyMySecondObjectConverter implements Converter<LinkedHashMap<String, String> , MySecondObject> {
 	@Override
     public MySecondObject convert(LinkedHashMap<String, String> sourceFromJson) {
-        if (sourceFromJson == null) {
-            return null;
-        }
-        return copyHashMapContentIntoEquivalentMySecondObject(sourceFromJson);
-    }
-	
-	//note : could be merged with similar method in MyMyObjectConverter
-    private MySecondObject copyHashMapContentIntoEquivalentMySecondObject(LinkedHashMap<String, String> myObjectFromJson) {
-    	final ObjectMapper mapper = new ObjectMapper();
-        return mapper.convertValue(myObjectFromJson, MySecondObject.class);
+        return (MySecondObject) MyObjectConverterUtil.copyHashMapContentIntoEquivalentMyObject(sourceFromJson, MySecondObject.class);
     }
 }
